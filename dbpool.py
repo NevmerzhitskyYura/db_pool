d7cac608d809269980cea17330ea8ac6b23464bb
Nevmerzhitsky Yura <nevmerzhitsky.jyra@gmail.com>
2021-03-22 20:13:15 +0200

    Refactor

--- dbpool.py
+++ dbpool.py
@@ -1,13 +1,16 @@
 import threading
 import time
 from contextlib import contextmanager
+import logging
 
 import psycopg2
 
 from db_settings import db_settings as dbs
 
-pool_instance = None
-pool_delay = 0.1
+pool_delay = 0.01
+
+#logging.basicConfig(level=logging.INFO, filename='dbpool.txt', filemode='w', format='%(message)s')
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
 class DataBasePool(object):
@@ -22,13 +25,22 @@ class DataBasePool(object):
         self._pool_size = pool_size
         self.connection_ttl = ttl
         self.lock = threading.RLock()
+        self.logging = logging.getLogger('dbpool')
+        self.logging.info("Create new pool")
+
+    def __del__(self):
+        self.logging.info('Close all connections in pool')
+        for connection in self._connection_pool:
+            self._close_connection(connection)
 
     def _create_connection(self):
         connection = psycopg2.connect(dbname=self._db_name, user=self._user, password=self._password, host=self._host)
+
         return {'connection': connection,
                 'creation_date': time.time()}
 
     def _close_connection(self, connection):
+        self.logging.info(f"Close connection with {id(connection)}")
         connection["connection"].close()
 
     def _get_connection(self):
@@ -36,8 +48,10 @@ class DataBasePool(object):
         while not connection:
             if self._connection_pool:
                 connection = self._connection_pool.pop()
+                logging.info(f'Get connection from pool with id {id(connection)}')
             elif len(self._connection_pool) < self._pool_size:
                 connection = self._create_connection()
+                self.logging.info(f"Create connection with id {id(connection)}")
             time.sleep(pool_delay)
         return connection
 
@@ -49,12 +63,19 @@ class DataBasePool(object):
 
         yield connection['connection']
 
-        if connection['creation_date'] + self.connection_ttl < time.time():
-            self._connection_pool.append(connection)
+
+        if connection['creation_date'] + self.connection_ttl > time.time():
+            self._push_to_pull(connection)
         else:
             self._close_connection(connection)
 
+    def _push_to_pull(self, connection):
+        self.logging.info(f'Push to the pull connetion with {id(connection)}')
+        self._connection_pool.append(connection)
 
 def pool_manager():
-    pool_instance = DataBasePool(**dbs, pool_size=10, ttl=1)
     return pool_instance
+
+
+pool_instance= DataBasePool(**dbs, pool_size=10, ttl=0.015)
+
--- test.py
+++ test.py
@@ -1,10 +1,52 @@
+import multiprocessing
+import unittest
+
+import psycopg2
+
 import dbpool as db
 
-with db.pool_manager().manager() as conn:
-    cursor = conn.cursor()
-    query = "select * from product"
-    cursor.execute(query)
-    users = cursor.fetchall()
 
-    for user in users:
-        print(user)
+def create_user():
+    with db.pool_manager().manager() as conn:
+        cursor = conn.cursor()
+        query = "INSERT INTO todolist_todolist (name, description) VALUES ('List1', 'About');"
+        cursor.execute(query)
+        conn.commit()
+
+
+def create_without_pool():
+    connection = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host='localhost')
+    cursor = connection.cursor()
+    query = "INSERT INTO todolist_todolist (name, description) VALUES ('List1', 'About');"
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+class UserTest(unittest.TestCase):
+    # def test1(self):
+    #     for _ in range(100):
+    #         create_user()
+
+    # def test2(self):
+    #     for _ in range(100):
+    #         create_without_pool()
+
+    # def test3(self):
+    #     processes = [multiprocessing.Process(target=create_user) for _ in range(1000)]
+    #     for process in processes:
+    #         process.start()
+    #     for process in processes:
+    #         process.join()
+
+    def test4(self):
+        processes = [multiprocessing.Process(target=create_without_pool) for _ in range(1000)]
+        for process in processes:
+            process.start()
+        for process in processes:
+            process.join()
+
+
+if __name__ == "__main__":
+    unittest.main()
